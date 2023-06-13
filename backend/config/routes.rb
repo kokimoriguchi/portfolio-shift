38823877a179650c2e2e00bc3232f165760b5078
kokimoriguchi <skoki5231@icloud.com>
2023-06-13 19:57:12 +0900

    DBミス修正

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # namespace :api do
-  #   namespace :v1, format: :json do
-  #     resources :コントローラー名
-  #   end
-  # end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
